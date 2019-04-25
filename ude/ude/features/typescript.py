38f134ad1472176376ee4d3bc5b0fb3eb0747d8e
leo <leo@leo-sa.com>
2019-04-25 03:54:20 -0500

    Fixed git tools, added Ag, and fixed coc install order

--- ude/ude/features/typescript.py
+++ ude/ude/features/typescript.py
@@ -4,7 +4,6 @@ from ..fs import run_cmd
 
 def setup(env: UdeEnvironment) -> UdeFeature:
     run_cmd(['npm', 'install', '-g', 'javascript-typescript-langserver'])
-    run_cmd(['nvim', '"+CocInstall coc-tsserver"', '+qall'])
     return UdeFeature(
         name='typescript',
         envs={}
