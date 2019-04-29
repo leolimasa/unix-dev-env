c1e79ded5c0bdb9b0b2964f499fafe0f3ff9a027
Leonardo Sa <leo@leo-sa.com>
2019-04-28 19:03:26 -0500

    Added tmux installation

--- ude/ude/systems/tmux.py
+++ ude/ude/systems/tmux.py
@@ -1,9 +1,14 @@
 import os
 from ..model import UdeFeature, UdeEnvironment
-from ..fs import run_cmd, rewrite_file_block, write_template
+from ..fs import run_cmd, rewrite_file_block, write_template, install_package
 
 
 def setup(env: UdeEnvironment) -> None:
+    try:
+        run_cmd(['which','tmux'])
+    except:
+        install_package('tmux')
+
     tmux_conf = os.path.join(env.ude_config_dir, "tmux.conf")
     rewrite_file_block("# UDE START\n", "# UDE END\n",
             os.path.join(env.home_dir, ".tmux.conf"),
