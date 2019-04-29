0c8f977d23094b95386421cebf386dda72274406
Leonardo Sa <leo@leo-sa.com>
2019-04-28 19:48:53 -0500

    Corrected fzf setup on linux

--- ude/ude/features/fzf.py
+++ ude/ude/features/fzf.py
@@ -1,13 +1,15 @@
 from ..model import UdeFeature, UdeEnvironment
 from ..fs import run_cmd, install_package
+from os import path
 import platform
+
 
 def setup(env: UdeEnvironment) -> UdeFeature:
     try:
         run_cmd(['which', 'fzf'])
     except:
         if platform.system() == 'Linux':
-            setup_linux()
+            setup_linux(env)
         elif platform.system() == 'Darwin':
             run_cmd(['brew', 'install', 'fzf'])
         else:
@@ -18,9 +20,9 @@ def setup(env: UdeEnvironment) -> UdeFeature:
             post_install=None
             )
 
-def setup_linux() -> None:
+def setup_linux(env: UdeEnvironment) -> None:
     run_cmd(['git','clone','--depth','1',
         'https://github.com/junegunn/fzf.git',
-        '~/.fzf'])
+        path.join(env.home_dir, ".fzf")])
     run_cmd(['~/.fzf/install'])
 
