d9879473cabc4913c260d34858604a1f9695a749
Leonardo Sa <leo@leo-sa.com>
2019-04-28 21:14:07 -0500

    added install node script

--- ude/ude/scripts/install_node.sh
+++ ude/ude/scripts/install_node.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-[[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh  # This loads NVM
+. $HOME/.nvm/nvm.sh  # This loads NVM
 nvm install stable
