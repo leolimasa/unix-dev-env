0f6ea851e7ad71b9deac2c5be8b04f07cb29a949
Leonardo Sa <leo@leo-sa.com>
2019-04-28 19:20:51 -0500

    Added installing python for linux

--- install.sh
+++ install.sh
@@ -2,5 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/ude
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	apt install python3.7
+	apt install python3-setuptools
+	apt install virtualenv
+	virtualenv venv
+	
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	# ...
+fi
+
 python3.7 setup.py develop
 python3.7 -m ude.install
