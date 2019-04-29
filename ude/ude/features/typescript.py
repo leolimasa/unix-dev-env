a74ca957369400be9bec8b309d809d630eeb007d
Leonardo Sa <leo@leo-sa.com>
2019-04-28 19:39:26 -0500

    Added node installation on linux

--- ude/ude/features/typescript.py
+++ ude/ude/features/typescript.py
@@ -1,8 +1,16 @@
 from ..model import UdeFeature, UdeEnvironment
 from ..fs import run_cmd
-
+import platform
+import os
 
 def setup(env: UdeEnvironment) -> UdeFeature:
+    try:
+        run_cmd(['which','node'])
+    except:
+        # TODO add OSX installation here as well (which is a PITA)
+        if platform.system() == 'Linux':
+            os.system('apt install nodejs')
+            os.system('apt install npm')
     run_cmd(['npm', 'install', '-g', 'javascript-typescript-langserver'])
     return UdeFeature(
         name='typescript',
