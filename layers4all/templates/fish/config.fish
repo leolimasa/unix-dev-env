810f7a3dc358697142df222398ec92a616d9c0d7
Leonardo Sa <leo@leo-sa.com>
2020-05-09 16:14:43 -0500

    gitmux config

--- layers4all/templates/fish/config.fish
+++ layers4all/templates/fish/config.fish
@@ -1,5 +1,9 @@
 
 set -x PATH ~/Projects/Sandbox/unix-dev-env/bin/ $PATH
+set -x PATH ~/.npm/bin/ $PATH
+if test (uname) = "Darwin"
+  set -x PATH ~/Projects/Sandbox/unix-dev-env/bin/mac $PATH
+end
 {% for i in injections.fish %}{{ i }}
 {% endfor %}
 source ~/.config/fish/local_config.fish
